abe036d6dc47d4b0fb3f4f3f2a3125331b19ba09
Ravinder Nath <ravi89nath@gmail.com>
2021-11-29 16:22:38 +0530

    Update ShowCredentials.java

--- GenerateCredential/src/CredentialService/ShowCredentials.java
+++ GenerateCredential/src/CredentialService/ShowCredentials.java
@@ -5,7 +5,7 @@ public class ShowCredentials {
 	public void showCredential(Employee employeeObject, String email, String generatedPassword) {
 	
 		System.out.println("Dear " + employeeObject .getFirstName() + "You credentials are: ");
-		System.out.println("Email is: " + email);
+		System.out.println("Email is: " + generatedEmailAddress);
 		System.out.println("Password is:" + generatedPassword);
 		
 	}
